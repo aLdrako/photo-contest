b4392632e90695394cb37a34d3ef8afe210e2a0c
Alexandr Gobjila <razielakaalien@outlook.com>
2023-04-17 19:08:18 +0300

    feat: Implemented ContestsView.html

--- src/main/java/com/telerikacademy/web/photocontest/controllers/mvc/ContestMvcController.java
+++ src/main/java/com/telerikacademy/web/photocontest/controllers/mvc/ContestMvcController.java
@@ -1,13 +1,22 @@
 package com.telerikacademy.web.photocontest.controllers.mvc;
 
 import com.telerikacademy.web.photocontest.helpers.AuthenticationHelper;
+import com.telerikacademy.web.photocontest.models.Contest;
+import com.telerikacademy.web.photocontest.models.dto.ContestResponseDto;
 import com.telerikacademy.web.photocontest.services.ModelMapper;
 import com.telerikacademy.web.photocontest.services.contracts.ContestServices;
 import com.telerikacademy.web.photocontest.services.contracts.PhotoServices;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping("/contests")
@@ -20,7 +29,11 @@ public class ContestMvcController extends BaseMvcController {
     private final ModelMapper modelMapper;
 
     @GetMapping
-    public String showAllContests() {
+    public String showAllContests(@RequestParam(required = false) Map<String, String> parameters, Model model, HttpSession session) {
+        Iterable<Contest> contests = contestServices.findAll();
+        List<ContestResponseDto> list = StreamSupport.stream(contests.spliterator(), false)
+                .map(modelMapper::objectToDto).toList();
+        model.addAttribute("contests", list);
         return "ContestsView";
     }
 }
