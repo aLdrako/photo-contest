45cceb10bd718dec6aeff7f4bda54c37be9e104a
LaptopARO <petrov.argir225@gmail.com>
2023-04-24 11:36:36 +0300

    add new script: textarea-expand small change in PhotoReviewDto

--- src/main/java/com/telerikacademy/web/photocontest/models/dto/PhotoReviewDto.java
+++ src/main/java/com/telerikacademy/web/photocontest/models/dto/PhotoReviewDto.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.*;
 public class PhotoReviewDto {
     @Min(value = 1, message = "Score must be between 1 and 10!")
     @Max(value = 10, message = "Score must be between 1 and 10!")
-    private int score;
+    private int score = 1;
     @NotEmpty(message = "A Comment must be present when reviewing a photo!")
     @Size(min = 20, max = 1024, message = "Size of a comment must be between 20 and 1024 characters!")
     private String comment;
